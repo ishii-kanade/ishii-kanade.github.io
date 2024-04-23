270f0935b26d160b896434ffb190f89b3bf1e2e0
石井 奏 <112748642+ishii-kanade@users.noreply.github.com>
2024-04-23 18:38:54 +0900

    視覚効果追加

--- scripts.js
+++ scripts.js
@@ -6,29 +6,42 @@ document.addEventListener("DOMContentLoaded", function () {
         '<i class="fas fa-envelope"></i> ' + user + "@" + domain;
 });
 
-document.getElementById('passwordForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // フォームのデフォルト送信を防ぐ
-    var password = document.getElementById('passwordInput').value;
-    fetch('https://api.laamile.com/api/password', { // URLを修正
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({password: password})
-    }).then(function(response) {
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error('サーバーからエラーが返されました。');
-        }
-    }).then(function(data) {
-        // URLが返されたら、そこにリダイレクト
-        window.location.href = data.url;
-    }).catch(function(error) {
-        console.error('Error:', error);
-        alert(error); // エラーをアラート表示
-    });
+document.getElementById('passwordInput').addEventListener('input', function (event) {
+    var passwordInput = event.target;
+    var password = passwordInput.value;
+    if (password.length === 8) {  // パスワードが8文字になった時にリクエストを送信
+        fetch('https://api.laamile.com/api/password', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ password: password })
+        })
+            .then(function (response) {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('サーバーからエラーが返されました。');
+                }
+            })
+            .then(function (data) {
+                // URLが返されたら、そこにリダイレクト
+                window.location.href = data.url;
+            })
+            .catch(function (error) {
+                console.error('Error:', error);
+                alert(error.message); // ブラウザのアラートでエラーメッセージを表示
+                passwordInput.value = ''; // パスワード入力欄を空にする
+                passwordInput.classList.add('is-invalid'); // 入力欄を赤くする
+            });
+    } else {
+        passwordInput.classList.remove('is-invalid'); // パスワード長が8未満のときは赤い枠を削除
+    }
 });
+
+
+
+
 
 
 
