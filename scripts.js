7d8e246c2fcf68500b98498365002e0d3ce39eeb
石井 奏 <112748642+ishii-kanade@users.noreply.github.com>
2024-04-23 13:46:37 +0900

    contact info secret

--- scripts.js
+++ scripts.js
@@ -6,9 +6,22 @@ document.addEventListener("DOMContentLoaded", function () {
     element.innerHTML = '<i class="fas fa-envelope"></i> ' + user + "@" + domain;
 });
 
-function checkPassword() {
-    var password = document.getElementById("passwordInput").value;
-    if (password === "19981225") {
-        window.location.href = "https://line.me/R/ti/p/XK-Z_Y4dxT";
+document.getElementById('passwordInput').addEventListener('input', function() {
+    var correctPassword = '19981225';
+    var input = this.value;
+    var lengthToCheck = Math.min(input.length, correctPassword.length);  // 入力または正しいパスワードの短い方の長さ
+    var correctCount = 0;
+
+    for (var i = 0; i < lengthToCheck; i++) {
+        if (input[i] === correctPassword[i]) correctCount++;
     }
-}
+
+    var maxBlur = 10;  // 最大ぼかし効果
+    var blur = maxBlur * (1 - correctCount / correctPassword.length);  // 一致する割合に基づいてぼかしを減らす
+
+    document.getElementById('contactInfo').style.filter = `blur(${blur}px)`;
+});
+
+
+
+
