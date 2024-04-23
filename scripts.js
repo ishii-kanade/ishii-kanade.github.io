d2518e2056cf0741d623ab6bd9ceb86c96586753
石井 奏 <112748642+ishii-kanade@users.noreply.github.com>
2024-04-23 18:01:25 +0900

    サーバーで処理

--- scripts.js
+++ scripts.js
@@ -6,12 +6,35 @@ document.addEventListener("DOMContentLoaded", function () {
         '<i class="fas fa-envelope"></i> ' + user + "@" + domain;
 });
 
-function checkPassword() {
-    var password = document.getElementById("passwordInput").value;
-    if (password === "19981225") {
-        window.location.href = "https://line.me/R/ti/p/XK-Z_Y4dxT";
-    }
-}
+document.getElementById('passwordForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // フォームのデフォルト送信を防ぐ
+    var password = document.getElementById('passwordInput').value;
+    fetch('https://api.laamile.com/api/password', { // URLを修正
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({password: password})
+    }).then(function(response) {
+        if (response.ok) { // ステータスコードが200の場合
+            return response.json(); // JSONレスポンスをパース
+        } else {
+            throw new Error('サーバーからエラーが返されました。');
+        }
+    }).then(function(data) {
+        // 応答の内容に応じた処理
+        if (data.redirect) {
+            window.location.href = data.url; // URLが指定されていればリダイレクト
+        } else {
+            alert(data.message); // メッセージを表示
+        }
+    }).catch(function(error) {
+        console.error('Error:', error);
+        alert(error); // エラーをアラート表示
+    });
+});
+
+
 
 
 
