09c117d3de5196c4d57170bc398a380c3b469b41
石井 奏 <112748642+ishii-kanade@users.noreply.github.com>
2024-04-25 09:19:48 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,50 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var user = "ishii-kanade";
-    var domain = "laamile.com";
+    // APIのエンドポイント（適宜変更してください）
+    var apiUrl = 'https://api.laamile.com/api/email';
+
+    // メールアドレスを表示するHTML要素を取得
     var element = document.getElementById("email");
-    element.innerHTML =
-        '<i class="fas fa-envelope"></i> ' + user + "@" + domain;
-});
 
-document.getElementById('passwordInput').addEventListener('input', function (event) {
-    var passwordInput = event.target;
-    var password = passwordInput.value;
-    if (password.length === 8) {  // パスワードが8文字になった時にリクエストを送信
-        fetch('https://api.laamile.com/api/password', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ password: password })
+    // APIからデータを取得する
+    fetch(apiUrl)
+        .then(response => {
+            // レスポンスをテキスト形式で取得
+            return response.text();
         })
-            .then(function (response) {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error('サーバーからエラーが返されました。');
-                }
-            })
-            .then(function (data) {
-                // URLが返されたら、そこにリダイレクト
-                window.location.href = data.url;
-            })
-            .catch(function (error) {
-                console.error('Error:', error);
-                alert(error.message); // ブラウザのアラートでエラーメッセージを表示
-                passwordInput.value = ''; // パスワード入力欄を空にする
-                passwordInput.classList.add('is-invalid'); // 入力欄を赤くする
-            });
-    } else {
-        passwordInput.classList.remove('is-invalid'); // パスワード長が8未満のときは赤い枠を削除
-    }
+        .then(email => {
+            // メールアドレスをHTML要素に挿入
+            element.innerHTML = '<i class="fas fa-envelope"></i> ' + email;
+        })
+        .catch(error => {
+            console.error('Error fetching email:', error);
+            element.innerHTML = '<i class="fas fa-envelope"></i> Error loading email';
+        });
 });
-
-
-
-
-
-
-
-
-
-
