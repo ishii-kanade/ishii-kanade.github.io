f30b735f40167614f6a2d527b27c423ceb2e2f3a
Ishii Kanade <ishii-kanade@laamile.com>
2024-10-02 22:48:06 +0900

    サイト名変更

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Portfolio',
+      title: 'laamile',
       theme: ThemeData(
         textTheme: GoogleFonts.notoSansJpTextTheme(),
         primarySwatch: Colors.blue,
